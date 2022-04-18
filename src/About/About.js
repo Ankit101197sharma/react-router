225386bea75728b5a9bcce7dda1d1e040abaaf7b
ankit101197sharma <ankit101197sharma@gmail.com>
2022-04-18 18:26:24 +0530

    Initial commit

---
+++ src/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import classes from "./About.module.css"
+
+const About=()=> {
+  return (
+    <div className={classes.aboutClass}>
+      
+     
+      <h2>This is the About Page</h2></div>
+  )
+}
+
+export default About;
---
+++ src/Contact/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from "./Contact.module.css"
+
+const Contact=()=> {
+  return (
+    <div className={classes.contactClass}>
+        <h2>This is The Contact Page</h2>
+        
+    </div>
+    
+  )
+}
+
+export default Contact;
